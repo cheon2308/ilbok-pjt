1023dcb60b96294f9daf3a6a724dd6976a64b7ad
cheon2308 <cjsgusxo@gmail.com>
2023-03-10 17:56:05 +0900

    front

---
+++ front-server/ilbok/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { GlobalStyle } from "./global-style";
+import AppRouter from "./AppRouter";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
---
+++ front-server/ilbok/src/AppRouter.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import JobMain from "./pages/Job/JobMain";
+import NavBar from "./component/NavBar";
+import WelfareMain from "./pages/Welfare/WelfareMain";
+import Main from "./pages/Main";
+
+const AppRouter = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/job/*" element={<JobMain />} />
+          <Route path="/welfare/*" element={<WelfareMain />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default AppRouter;
