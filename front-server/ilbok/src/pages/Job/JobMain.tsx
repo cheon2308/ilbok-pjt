337d21aabb5d4bff5876ec9cf4bdec93909936f3
cheon2308 <cjsgusxo@gmail.com>
2023-03-10 17:56:05 +0900

    front

---
+++ front-server/ilbok/src/pages/Job/JobMain.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function JobMain() {
+  return (
+    <>
+      <div>일자리 페이지</div>
+    </>
+  );
+}
+export default JobMain;
---
+++ front-server/ilbok/src/pages/Welfare/WelfareMain.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WelfareMain() {
+  return (
+    <>
+      <div>복지 페이지</div>
+    </>
+  );
+}
+
+export default WelfareMain;
