aa095ca785cbfd740429b8e34666f4aa21fe9e78
김현진 <kjinabc@gmail.com>
2023-03-16 16:46:15 +0900

    fix : kako login

--- front-server/src/api/KakaoLoginData.tsx
+++ front-server/src/api/KakaoLoginData.tsx
@@ -1,2 +1,2 @@
 export const REST_API_KEY = 'bada3164a5bba8db1d10012378cc51aa'
-export const REDIRECT_URI = 'http://localhost:8080/oauth2/login/kakao'
+export const REDIRECT_URI = 'http://localhost:3000/oauth2/login/kakao'
