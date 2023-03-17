0ac42d5af91cf37ce5059bf674a1be49202c6404
youmean0427 <youmean0427@naver.com>
2023-03-17 17:40:26 +0900

    feat: Detail Page CSS
    Former-commit-id: 23226cd8035ecfc6d72f490ed3dff12be2b22ce8

---
+++ front-server/src/pages/Job/JobDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobDetailItem from '../../components/Job/JobDetailItem'
+import './JobDetail.css'
+export default function JobDetail() {
+  return (
+    <>
+      <div className="Main-container">
+        <JobDetailItem />
+      </div>
+    </>
+  )
+}
