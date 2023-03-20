19b645b794066f22ddeac08356c44c12d2574768
LJH <pliot123@naver.com>
2023-03-20 15:03:02 +0900

    fix: login
    Former-commit-id: eccb6e90ee19337a31f928aae69ff1d855844027

--- back-server/src/main/java/com/ssafy/e202/ilbok/controller/UserController.java
+++ back-server/src/main/java/com/ssafy/e202/ilbok/controller/UserController.java
@@ -2,29 +2,30 @@ package com.ssafy.e202.ilbok.controller;
 
 import com.ssafy.e202.ilbok.model.dto.OauthToken;
 import com.ssafy.e202.ilbok.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@ComponentScan
 @RestController //(1)
-@RequestMapping("/api")
+@RequestMapping()
 public class UserController {
 
     @Autowired
     private UserService userService; //(2)
 
     // 프론트에서 인가코드 받아오는 url
-    @GetMapping("/oauth/token") // (3)
-    public OauthToken getLogin(@RequestParam("code") String code) { //(4)
+    @GetMapping("/oauth") // (3)
+    public void getLogin() { //(4)
+        System.out.println("현재 코드");
 
         // 넘어온 인가 코드를 통해 access_token 발급 //(5)
-        OauthToken oauthToken = userService.getAccessToken(code);
+//        OauthToken oauthToken = userService.getAccessToken(code);
 
-        return oauthToken;
+//        return oauthToken;
     }
 
 }
--- front-server/src/components/KakaoLogin.tsx
+++ front-server/src/components/KakaoLogin.tsx
@@ -1,22 +1,27 @@
+import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router'
 import { REST_API_KEY, REDIRECT_URI } from '../api/KakaoLoginData'
+
 function KakaoLogin() {
   const location = useLocation()
   const navigate = useNavigate()
   const KAKAO_CODE = location.search.split('=')[1]
   const getKAkaoToken = () => {
-    fetch(`http://localhost:8080/oauth?code=${KAKAO_CODE}`, {
+    console.log(KAKAO_CODE);
+    fetch(`http://localhost:8080/oauth`, {
       method: 'GET',
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.access_token) {
-          localStorage.setItem('token', data.access_token)
-        } else {
-          navigate('/')
-        }
-      })
+    .then((res) => console.log(res));
+
+      // .then((res) => res.json())
+      // .then((data) => {
+      //   if (data.access_token) {
+      //     localStorage.setItem('token', data.access_token)
+      //   } else {
+      //     navigate('/')
+      //   }
+      // })
   }
   useEffect(() => {
     // console.log(KAKAO_CODE)
