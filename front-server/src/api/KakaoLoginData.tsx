d14d5181f22719290ebccb465e26538bc393fd2c
LJH <pliot123@naver.com>
2023-03-20 15:03:02 +0900

    fix: login
    Former-commit-id: 5231ab88a2f9b309a52c30946decd6a4f6ef5997

--- front-server/src/api/KakaoLoginData.tsx
+++ front-server/src/api/KakaoLoginData.tsx
@@ -1,2 +1,2 @@
-export const REST_API_KEY = 'bada3164a5bba8db1d10012378cc51aa'
-export const REDIRECT_URI = 'http://localhost:3000/oauth2/login/kakao'
+export const REST_API_KEY = 'fb9737b606aa1abe22c3b71a9e42b209'
+export const REDIRECT_URI = 'http://localhost:3000/oauth'
