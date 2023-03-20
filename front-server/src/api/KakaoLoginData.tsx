c220892319892f6a9ddabce232a69429176af3c5
김현진 <kjinabc@gmail.com>
2023-03-20 14:49:00 +0900

    s

--- front-server/src/api/KakaoLoginData.tsx
+++ front-server/src/api/KakaoLoginData.tsx
@@ -1,2 +1,3 @@
-export const REST_API_KEY = 'bada3164a5bba8db1d10012378cc51aa'
-export const REDIRECT_URI = 'http://localhost:3000/oauth2/login/kakao'
+export const REST_API_KEY = 'fb9737b606aa1abe22c3b71a9e42b209'
+export const REDIRECT_URI = 'http://localhost:3000/oauth'
+// https://kauth.kakao.com/oauth/logout?client_id=bada3164a5bba8db1d10012378cc51aa&logout_redirect_uri=${LOGOUT_REDIRECT_URI}
