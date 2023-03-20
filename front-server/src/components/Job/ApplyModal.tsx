af38898c878b8de49f93e80b67e738349b2193cf
youmean0427 <youmean0427@naver.com>
2023-03-20 10:25:32 +0900

    Feat: Footer
    Former-commit-id: da61b791402b808466e6902dc1eef9d28375f73c

--- front-server/src/components/Job/ApplyModal.tsx
+++ front-server/src/components/Job/ApplyModal.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './ApplyModal.css'
 import square from '../../assets/image/Square.png'
 type Props = {
-  open?: any
+  open?: boolean
   close?: any
 }
 export default function ApplyModal({ open, close }: Props) {
@@ -11,35 +11,35 @@ export default function ApplyModal({ open, close }: Props) {
       {open ? (
         <section>
           <main className="modal-main">
-            <div className='Modal-container'>
+            <div className="Modal-container">
               <div>
-                <div className='Modal-title'>
-                <div className="Big-category">전형방법</div>
-                <button onClick={close}>&times;</button>
+                <div className="Modal-title">
+                  <div className="Big-category">전형방법</div>
+                  <button onClick={close}>&times;</button>
                 </div>
                 <div>
-                <img className="square" src={square} alt="" />
-                <hr />
+                  <img className="square" src={square} alt="" />
+                  <hr />
                 </div>
               </div>
-
-                <div className="Modal-line-container">   
-                    <div className="Mid-category">접수마감</div>
-                    <div>데이터</div>
-                </div >
-                <div className="Modal-line-container">  
-                    <div className="Mid-category">전형방법</div>
-                    <div>데이터</div>
-                </div >
-                <div className="Modal-line-container">  
-                    <div className="Mid-category">접수방법</div>
-                    <div>데이터</div>
+              <div>
+                <div className="Modal-line-container">
+                  <div className="Mid-category Modal-flexgrow1">접수마감</div>
+                  <div className="Modal-flexgrow2">데이터</div>
                 </div>
-                <div className="Modal-line-container">  
-                    <div className="Mid-category">제출서류</div>
-                    <div>데이터</div>
+                <div className="Modal-line-container">
+                  <div className="Mid-category Modal-flexgrow1">전형방법</div>
+                  <div className="Modal-flexgrow2">데이터</div>
                 </div>
-              
+                <div className="Modal-line-container">
+                  <div className="Mid-category Modal-flexgrow1">접수방법</div>
+                  <div className="Modal-flexgrow2">데이터</div>
+                </div>
+                <div className="Modal-line-container">
+                  <div className="Mid-category Modal-flexgrow1">제출서류</div>
+                  <div className="Modal-flexgrow2">데이터</div>
+                </div>
+              </div>
             </div>
           </main>
         </section>
