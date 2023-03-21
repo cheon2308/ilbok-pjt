955ca039a27301ab1860d60aa3c84e358f8a01c0
kjinabc <kjinabc@gmail.com>
2023-03-21 19:41:17 +0900

    Design: Myprofile CSS
    Former-commit-id: 45ed63900929d1ee2a358a4ee7976186bf91b9a1

--- front-server/src/pages/MyProfile/MyProfile.tsx
+++ front-server/src/pages/MyProfile/MyProfile.tsx
@@ -2,7 +2,36 @@ import React from 'react'
 import '../../assets/styles/Myprofile/MyProfile.css'
 
 function MyProfile() {
-  return <div className="Profilre-Main-container"></div>
+  return (
+    <>
+      <div className="Profile-Main-container">
+        <div className="Profile-Info-container">
+          <div className="Profile-Profile-container">
+            <div>프로필사진</div>
+            <div>안녕하세요, 김현진님</div>
+          </div>
+          <div>
+            <div>개인정보수정</div>
+            <div>개인이력수정</div>
+          </div>
+        </div>
+        <div className="Profile-Chart-container">
+          <div>차트1</div>
+          <div>차트2</div>
+        </div>
+      </div>
+      <div className="Profile-Main-container Profile-Reco-container">
+        <div>김현진님과 어울리는 일자리</div>
+        <div>일복에서 추천하는 어울리는 일자리</div>
+        <div>캐러셀</div>
+      </div>
+      <div className="Profile-Main-container Profile-Like-container">
+        <div>김현진님이 관심있는 일자리</div>
+        <div>일복이 추천하는 일자리</div>
+        <div>게시판</div>
+      </div>
+    </>
+  )
 }
 
 export default MyProfile
