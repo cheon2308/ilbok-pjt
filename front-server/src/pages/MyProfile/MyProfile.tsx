477fed801dd20ca1eb48f15913b5bd68043bbebf
김현진 <kjinabc@gmail.com>
2023-03-21 17:55:59 +0900

    feat : myprofilepage
    Former-commit-id: 1907201532b84645ed84274af93286edb83b559a

---
+++ front-server/src/pages/MyProfile/MyProfile.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../assets/styles/Myprofile/MyProfile.css'
+
+function MyProfile() {
+  return <div className="Profilre-Main-container"></div>
+}
+
+export default MyProfile
