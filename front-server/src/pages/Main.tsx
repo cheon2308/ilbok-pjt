da76aa744c1705b200ce1962ef3a80daf1a9842a
youmean0427 <youmean0427@naver.com>
2023-03-21 14:52:52 +0900

    Design: Main Page
    Former-commit-id: 9088111bc4f68464511e5d6cd6992cdeac39ee24

--- front-server/src/pages/Main.tsx
+++ front-server/src/pages/Main.tsx
@@ -5,38 +5,76 @@ import CarouselComponent from '../components/Common/Carousel'
 import styled from 'styled-components'
 import mainImage from '../assets/image/mainImage.png'
 import { useState } from 'react'
+import Footer from '../components/Common/Footer'
+import Select from 'react-select'
+
+const Ilbok = styled.div`
+  margin: 0 12vw 0 12vw;
+`
 
 const IlbokMain = styled.div`
+  height: 450px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const IlbokMainContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: row;
   height: 400px;
+  width: 100%;
 `
 
-const IlbokMainImg = styled.img`
-  width: 15%;
-  margin: 20px 10px;
+const IlbokImgContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 50%;
+
+  margin-right: 50px;
 `
+const IlbokMainImg = styled.img`
+  width: 250px;
+`
+
 const IlbokTitleContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  width: 50%;
+
+  margin-left: 50px;
+`
+const SearchContanier = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const IlbokMainTitle = styled.div`
   font-size: 40px;
-  font-weight: bold;
+  font-weight: 800;
+  color: #76dcb0;
   margin-top: 20px;
+  width: 100%;
+  text-align: left;
 `
 
-const IlbokMainSubtitle = styled.div`
+const IlbokMainSubtitle = styled.span`
   font-size: 20px;
-  font-weight: bold;
+  font-weight: 700;
+  color: #666666;
   margin-top: 10px;
+  margin-bottom: 5px;
+  width: 100%;
+  text-align: left;
 `
-
+const StyledSelect = styled(Select)`
+  width: 150px;
+`
 const MainPage = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0)
 
@@ -54,13 +92,23 @@ const MainPage = () => {
 
   return (
     <>
-      <IlbokMain>
-        <IlbokMainImg src={mainImage} alt="mainImg" />
-        <IlbokTitleContainer>
-          <IlbokMainTitle>일복</IlbokMainTitle>
-          <IlbokMainSubtitle>나만의 일자리/복지</IlbokMainSubtitle>
-        </IlbokTitleContainer>
-      </IlbokMain>
+      <Ilbok>
+        <IlbokMain>
+          <IlbokMainContainer>
+            <IlbokImgContainer>
+              <IlbokMainImg src={mainImage} alt="mainImg" />
+            </IlbokImgContainer>
+            <IlbokTitleContainer>
+              <IlbokMainTitle>일복(日福)</IlbokMainTitle>
+              <br />
+
+              <IlbokMainSubtitle>나만의 일자리/복지</IlbokMainSubtitle>
+
+              <IlbokMainSubtitle> 알맞은 일자리와 복지를 제공합니다.</IlbokMainSubtitle>
+            </IlbokTitleContainer>
+          </IlbokMainContainer>
+        </IlbokMain>
+      </Ilbok>
       <div
         style={{
           display: 'flex',
@@ -68,22 +116,28 @@ const MainPage = () => {
           alignItems: 'center',
           height: '200px',
           textAlign: 'center',
-          marginTop: '30px',
-          backgroundColor: '#C6F0DE',
+          backgroundColor: '#e7f4ef',
         }}
       >
-        <SearchBar width="60%" height="30px" placeholder="Search" />
+        <Ilbok>
+          <SearchContanier>
+            <SearchBar width="800px" height="30px" placeholder="검색어를 입력하세요." />
+          </SearchContanier>
+        </Ilbok>
       </div>
+      <Ilbok>
+        <div style={{ marginTop: '30px' }}>
+          <CarouselComponent
+            items={items.map((item) => (
+              <Card key={item.title} title={item.title} description={item.description} image={item.image} />
+            ))}
+            activeIndex={activeIndex}
+            onChange={handleItemChange}
+          />
+        </div>
+      </Ilbok>
 
-      <div style={{ marginTop: '30px' }}>
-        <CarouselComponent
-          items={items.map((item) => (
-            <Card key={item.title} title={item.title} description={item.description} image={item.image} />
-          ))}
-          activeIndex={activeIndex}
-          onChange={handleItemChange}
-        />
-      </div>
+      <Footer />
     </>
   )
 }
