09d3ddd8e7471d948910074a9a19734d58359ae2
LJH <pliot123@naver.com>
2023-03-21 15:22:12 +0900

    feat: kakao login
    Former-commit-id: 2ff9ac2f6f7d0474c85000287596258e50db5d0d

--- back-end/src/main/java/com/ssafy/ilbok/service/UserService.java
+++ back-end/src/main/java/com/ssafy/ilbok/service/UserService.java
@@ -3,7 +3,9 @@ package com.ssafy.ilbok.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.ilbok.Repository.UserRepository;
+import com.ssafy.ilbok.model.dto.KakaoProfile;
 import com.ssafy.ilbok.model.dto.OauthToken;
+import com.ssafy.ilbok.model.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -35,7 +37,6 @@ public class UserService {
         params.add("client_id", "fb9737b606aa1abe22c3b71a9e42b209");
         params.add("redirect_uri", "http://localhost:3000/oauth");
         params.add("code", code);
-        params.add("client_secret", "{시크릿 키}"); // 생략 가능!
 
         //(5)
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
@@ -49,6 +50,7 @@ public class UserService {
                 String.class
         );
 
+
         //(7)
         ObjectMapper objectMapper = new ObjectMapper();
         OauthToken oauthToken = null;
@@ -57,7 +59,73 @@ public class UserService {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
+        System.out.println("나");
+        System.out.println(oauthToken);
 
         return oauthToken; //(8)
     }
+
+
+
+    public User saveUser(String token) {
+
+        //(1)
+        KakaoProfile profile = findProfile(token);
+
+        //(2)
+        User user = userRepository.findByKakaoEmail(profile.getKakao_account().getEmail());
+
+        //(3)
+        if(user == null) {
+            user = User.builder()
+                    .kakaoId(profile.getId())
+                    //(4)
+                    .kakaoProfileImg(profile.getKakao_account().getProfile().getProfile_image_url())
+                    .kakaoNickname(profile.getKakao_account().getProfile().getNickname())
+                    .kakaoEmail(profile.getKakao_account().getEmail())
+                    //(5)
+                    .userRole("ROLE_USER").build();
+
+            userRepository.save(user);
+        }
+
+        return user;
+    }
+
+    //(1-1)
+    public KakaoProfile findProfile(String token) {
+
+        //(1-2)
+        RestTemplate rt = new RestTemplate();
+
+        //(1-3)
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token); //(1-4)
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        //(1-5)
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest =
+                new HttpEntity<>(headers);
+
+        //(1-6)
+        // Http 요청 (POST 방식) 후, response 변수에 응답을 받음
+        ResponseEntity<String> kakaoProfileResponse = rt.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest,
+                String.class
+        );
+
+        //(1-7)
+        ObjectMapper objectMapper = new ObjectMapper();
+        KakaoProfile kakaoProfile = null;
+        try {
+            kakaoProfile = objectMapper.readValue(kakaoProfileResponse.getBody(), KakaoProfile.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return kakaoProfile;
+    }
+
 }
