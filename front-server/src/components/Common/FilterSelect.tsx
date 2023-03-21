ae48cf485782885b274d0fa3fb97abe9b68f7d2e
youmean0427 <youmean0427@naver.com>
2023-03-21 19:52:36 +0900

    Feat: Main Page Select
    Former-commit-id: 0aaddd17a23a4c7b7ae7ce7f3b6c9406bf0e97f4

--- front-server/src/components/Common/FilterSelect.tsx
+++ front-server/src/components/Common/FilterSelect.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const FilterSelect = (props: []) => {
+interface Props {
+  name: string
+}
+
+interface ComponentProps {
+  props: Array<Props>
+}
+
+const FilterSelectWrapper = styled.div`
+  width: 100%;
+  max-width: 378px;
+  margin-right: 2em;
+`
+
+const StyledSelect = styled.select`
+  width: 100%;
+  height: 42px;
+  padding: 0 14px;
+  border: 1px solid #d7e2eb;
+  border-radius: 4px;
+  box-sizing: border-box;
+  background-size: 0.625rem 0.3125rem;
+  background-color: #fbfbfd;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.6;
+  color: #263747;
+  &:hover {
+    border: 1px solid #848484;
+    box-shadow: inset 0 0 0 1px #bcb7d9;
+  }
+`
+
+const FilterSelect = ({ props }: ComponentProps) => {
   return (
-    <div>
-      {props.map((ele: string, i: number) => (
-        <option key={i} value={ele}>
-          {ele}
-        </option>
-      ))}
-    </div>
+    <FilterSelectWrapper>
+      <StyledSelect>
+        {props.map((ele: Props, i: number) => (
+          <option key={i} value={ele.name}>
+            {ele.name}
+          </option>
+        ))}
+      </StyledSelect>
+    </FilterSelectWrapper>
   )
 }
+
 export default FilterSelect
