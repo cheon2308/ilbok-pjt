d4903f2a7deaac26a3580bcf5216b25ef9d006ad
cheon2308 <cjsgusxo@gmail.com>
2023-03-22 17:58:04 +0900

    readme
    Former-commit-id: eb30420a93d98dfef135bfa4617d8fa5ed72bc70

--- django/jobdata/views.py
+++ django/jobdata/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-from .models import JobFamily
+from .models import *
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
 # Create your views here.
 
-def job_family(request):
-    families = JobFamily.objects.all()
+# 
+# def jobCodeMatch(request):
+#     job_cat
+
+def jobList(request):
+    families = Wanted.objects.all()
+    jobCode = set()
+    for i in families:
+        jobCode.add(i.job_code)
     return render(request, 'index.html', {"families": families})
+
+# 코사인 유사도
+def cos_sim(A, B):
+    return dot(A, B) / (norm(A)*norm(B))
+
+def job_to_job(request):
+    dataset = All_in_one.objects.all()
+    print(dataset,333333333333333333333)
+    return render(request, 'test.html', {"dataset": dataset})
+
+doc1 = np.array([0,1,1,1])
+doc2 = np.array([1,0,1,1])
+doc3 = np.array([2,0,2,2])
+
+print('문서 1과 문서2의 유사도 :',cos_sim(doc1, doc2))
+print('문서 1과 문서3의 유사도 :',cos_sim(doc1, doc3))
+print('문서 2와 문서3의 유사도 :',cos_sim(doc2, doc3))
+print(norm(doc3))
---
+++ django/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ilbok',
+        'USER': 'root',
+        'PASSWORD': 'Snrnsep33!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-jz4ioly_)cu0q&(2p+jm#ksvse)5k#3uwid%l6f9!)auanpkv5'
