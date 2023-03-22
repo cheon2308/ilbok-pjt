8376d908ee8a09338a30c2b86d4084f9cfc835e0
cheon2308 <cjsgusxo@gmail.com>
2023-03-22 09:17:27 +0900

    feat: django algo
    Former-commit-id: 9be8b3f644db3fa9145cd12555c5a039e31c28bb

--- django/jobdata/views.py
+++ django/jobdata/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import JobFamily
 # Create your views here.
+
+def job_family(request):
+    families = JobFamily.objects.all()
+    return render(request, 'index.html', {"families": families})
