1f26f081d917f46bca757f17567a5828bc9c3911
cheon2308 <cjsgusxo@gmail.com>
2023-03-24 17:21:38 +0900

    Feat: django job to job cos_similar
    Former-commit-id: b6688c9b833299a8ec106ea13a13645fdf5adeed [formerly c2a138de000306585eba12c4ae7cb0df5d0ee6e4]
    Former-commit-id: f66e37d8c9f758f313005dd718985abbd134c98b

--- django/jobdata/urls.py
+++ django/jobdata/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from jobdata import views
 
 urlpatterns = [
-    path('', views.jobList),
-    path('test', views.job_info),
+    path('makejobmatrix', views.job_info),
+    path('<int:wanted_job_num>', views.load_sim_job ),
+    path('data/<int:wanted_job_num>', views.similar_job )
 ]
