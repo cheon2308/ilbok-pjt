6aa5e9681e15c40ef3eae2757f2d74198417244f
이재호 <pliot123@naver.com>
2023-03-27 00:04:32 +0900

    feat: 상재가 내준 숙제
    Former-commit-id: 4de62f808300efc6889b53033e09cd512e04bab3

--- back-end/src/main/java/com/ssafy/ilbok/model/entity/Careers.java
+++ back-end/src/main/java/com/ssafy/ilbok/model/entity/Careers.java
@@ -3,10 +3,7 @@ package com.ssafy.ilbok.model.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -14,7 +11,7 @@ import javax.persistence.Table;
 @Table(name = "careers")
 public class Careers {
 
-    @Id
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "user_id")
