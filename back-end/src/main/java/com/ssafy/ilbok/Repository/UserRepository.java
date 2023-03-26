221dad6a2ff6dd412c6b9d781f125d7078d9f87f
이재호 <pliot123@naver.com>
2023-03-27 00:04:32 +0900

    feat: 상재가 내준 숙제
    Former-commit-id: 965f54e80cba0cde7fc40c61da49890b60ee6a58

--- back-end/src/main/java/com/ssafy/ilbok/Repository/UserRepository.java
+++ back-end/src/main/java/com/ssafy/ilbok/Repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.ssafy.ilbok.Repository;
 
 import com.ssafy.ilbok.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     // JPA findBy 규칙
     // select * from user_master where kakao_email = ?
