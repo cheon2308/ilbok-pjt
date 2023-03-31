351bb95c3c15b25b70c791be22136d6364645bc8
김현진 <kjinabc@gmail.com>
2023-03-31 16:55:23 +0900

    s
    Former-commit-id: 9b165e9991f399b012017dc90433b83604384491 [formerly 5dea329d36d62e20f7015e3d5c76a2e24b2110ca]
    Former-commit-id: b55845e7b93273c49276a541cabf69cf85dafd66

--- front-server/src/components/Common/JobSelect/JobSubSelect2.tsx
+++ front-server/src/components/Common/JobSelect/JobSubSelect2.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 import React from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
 const JobSubFamilyItem = styled.div`
   padding: 15px 10px 15px 10px;
 `
+interface JobCodeItemtype {
+  jobFamilyCode: string
+  name: string
+}
 function JobSubSelect2({ jobSubSelectCode, jobSubSelect2NameFunc }: any) {
   const JobSubFamily = [
     { jobSubCode2: '11111', jobSubCode: '3331', jobFamilyCode: '123', name: '양식' },
@@ -13,8 +20,36 @@ function JobSubSelect2({ jobSubSelectCode, jobSubSelect2NameFunc }: any) {
   ]
 
   const [activeTab, setActiveTab] = React.useState('')
+  const [JobCode, setJobCode] = useState<JobCodeItemtype[]>()
 
+  const getJobCode = async () => {
+    const res = await axios(process.env.REACT_APP_SERVER_URL + `/resume/jobFamily`, {
+      method: 'GET',
+    })
+    return res.data
+  }
+
+  const { data, error, isError, isLoading } = useQuery(['getJobCode'], getJobCode, {
+    onSuccess: (data) => {
+      setJobCode(data)
+      console.log('data:', data)
+      // 데이터 로드 후 실행할 작업
+    },
+    onError: (error) => {
+      console.log('error:', error)
+      // 에러 발생 후 실행할 작업
+    },
+  })
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Error occurred </div>
+  }
   console.log(jobSubSelectCode)
+
   return (
     <>
       <div
