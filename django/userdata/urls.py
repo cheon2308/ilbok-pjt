005c7d3fd8e62e9c87fff0d67e98d37d6be39462
cheon2308 <cjsgusxo@gmail.com>
2023-03-31 17:24:04 +0900

    feat : als algo
    Former-commit-id: cc1a37d344dfcfe92ba15d65a1ef8dc9cc2d908c [formerly ef41b7e22ad20458d76cc465d9e355a60be6516d]
    Former-commit-id: 63e695e5927c54d0f5da0954c76cba317f203541

--- django/userdata/urls.py
+++ django/userdata/urls.py
@@ -6,6 +6,13 @@ from userdata import views
 app_name = 'userdata'
 urlpatterns = [
     path('makeusermatrix', views.user_info),
-    path('<int:user_num>', views.load_user_matrix ),
-    # path('data/<int:wanted_job_num>', views.similar_job )
+    # 이미 본 공고 포함 추천받기
+    path('<int:user_id>', views.recommend_items_for_user ),
+    # 이미 본 공고 제외 추천받기
+    path('except/<int:user_id>', views.recommend_items ),
+    # 로그 쌓인 후 행렬 업데이트
+    path('update', views.user_train),
+    # auc 커브로 성능확인하기
+    path('auc', views.check_calc_mean),
+
 ]
