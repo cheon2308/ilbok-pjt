a90e5805575415498c31e5bef15b8be2015f669a
cheon2308 <cjsgusxo@gmail.com>
2023-03-31 18:00:52 +0900

    feat: algo end
    Former-commit-id: 6a4ade1c178e3e0572371398a4ee1667b3e13cc0 [formerly eda751592ecca882f8c405d3c84e4c6f30fa8096]
    Former-commit-id: ca1602b8489f4a93ab30117069674ce94f108d84

--- django/userdata/urls.py
+++ django/userdata/urls.py
@@ -5,14 +5,19 @@ from userdata import views
 
 app_name = 'userdata'
 urlpatterns = [
-    path('makeusermatrix', views.user_info),
+    
     # 이미 본 공고 포함 추천받기
     path('<int:user_id>', views.recommend_items_for_user ),
     # 이미 본 공고 제외 추천받기
     path('except/<int:user_id>', views.recommend_items ),
     # 로그 쌓인 후 행렬 업데이트
-    path('update', views.user_train),
+    path('update/log', views.user_train),
     # auc 커브로 성능확인하기
     path('auc', views.check_calc_mean),
+    # 신규 유저, cold start 시 이력서 바탕으로 해주기
+    # 유저 - 유저간 행렬 업데이트 시켜주고 cold start 호출
+    # 이후 업데이트
+    path('update/usertouser', views.user_info),
+    path('cf/<int:user_id>', views.rec_cf_user),
 
 ]
