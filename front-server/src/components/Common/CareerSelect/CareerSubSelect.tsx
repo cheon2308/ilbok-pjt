acd9b4d63dc53a70fd3d960369bd09a5d636271b
youmean0427 <youmean0427@naver.com>
2023-04-03 04:04:35 +0900

    Feat: CareerInfo feature 완료
    Former-commit-id: 3b7c5c44e17972ec562e61e11de9911c3231f505 [formerly eba039e9dc2f840eb089022189eeb2ca8e51b2c8]
    Former-commit-id: 73c264b272ac2c24f2d87a1851547a67c42ce46e

--- front-server/src/components/Common/CareerSelect/CareerSubSelect.tsx
+++ front-server/src/components/Common/CareerSelect/CareerSubSelect.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 import { useRecoilState } from 'recoil'
-import { CareerSelectCode, CareerSubSelectName } from '../../../atom'
+import { CareerSelectCode, CareerSubSelectName, Period } from '../../../atom'
 
 const RegionFamilyItem = styled.div`
   padding: 15px 10px 15px 10px;
@@ -18,34 +18,34 @@ function CareerSubSelect() {
   const [activeTab, setActiveTab] = React.useState('')
   const [careerCode, setCarrerCode] = useRecoilState(CareerSelectCode)
   const [careerSubName, setCarrerSubName] = useRecoilState(CareerSubSelectName)
+  const [period, setPeriod] = useRecoilState(Period)
   const CareerSubFamily = [
-    { careerCode: '1', period: '1', name: '1년 미만' },
-    { careerCode: '1', period: '2', name: '1년 이상 ~ 3년 미만' },
-    { careerCode: '1', period: '3', name: '3년 이상 ~ 5년 미만' },
-    { careerCode: '1', period: '4', name: '5년 이상 ~ 10년 미만' },
-    { careerCode: '1', period: '5', name: '10년 이상' },
+    { period: '1', name: '1년 미만' },
+    { period: '2', name: '1년 이상 ~ 3년 미만' },
+    { period: '3', name: '3년 이상 ~ 5년 미만' },
+    { period: '4', name: '5년 이상 ~ 10년 미만' },
+    { period: '5', name: '10년 이상' },
   ]
 
   return (
     <>
       <div style={{ width: '50%' }}>
-        {careerCode === '1'
-          ? CareerSubFamily.map((item, index) => (
-              <RegionFamilyItem
-                key={index}
-                onClick={() => {
-                  setActiveTab(item.period)
-                  setCarrerSubName(item.name)
-                }}
-                style={{
-                  backgroundColor: activeTab === item.period ? '#76dcb0' : index % 2 !== 0 ? '#ffffff' : '#f2f2f2',
-                  color: activeTab === item.period ? '#ffffff' : '',
-                }}
-              >
-                {item.name}
-              </RegionFamilyItem>
-            ))
-          : null}
+        {CareerSubFamily.map((item, index) => (
+          <RegionFamilyItem
+            key={index}
+            onClick={() => {
+              setActiveTab(item.period)
+              setCarrerSubName(item.name)
+              setPeriod(item.period)
+            }}
+            style={{
+              backgroundColor: activeTab === item.period ? '#76dcb0' : index % 2 !== 0 ? '#ffffff' : '#f2f2f2',
+              color: activeTab === item.period ? '#ffffff' : '',
+            }}
+          >
+            {item.name}
+          </RegionFamilyItem>
+        ))}
       </div>
     </>
   )
