8a6f3a4d779de9aa38b1a25d5e4bac12342306b4
youmean0427 <youmean0427@naver.com>
2023-04-03 04:04:35 +0900

    Feat: CareerInfo feature 완료
    Former-commit-id: ee585df725ac0764b2f17e950dc02afc45786f6a [formerly f6b792dc3f9e705fd2264d364f2f0d253a4245b5]
    Former-commit-id: 451622d88cbc6d0dcc01516c40715a846be892f0

--- front-server/src/pages/MyProfile/CareerInfo.tsx
+++ front-server/src/pages/MyProfile/CareerInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import BokBtn1 from '../../components/Common/BokBtn1'
 import BokBtn2 from '../../components/Common/BokBtn2'
@@ -9,22 +9,34 @@ import JobSelect from '../../components/Common/JobSelect/JobSelect'
 import JobSubSelect from '../../components/Common/JobSelect/JobSubSelect'
 import JobSubSelect2 from '../../components/Common/JobSelect/JobSubSelect2'
 import { SlMagnifier } from 'react-icons/sl'
+import { HiPlus } from 'react-icons/hi'
 import RegionSelect from '../../components/Common/RegionSelect/RegionSelect'
 import CitySelect from '../../components/Common/RegionSelect/CitySelect'
-import CareerSelect from '../../components/Common/CareerSelect/CareerSelect'
+import { CgClose } from 'react-icons/cg'
 import CareerSubSelect from '../../components/Common/CareerSelect/CareerSubSelect'
-import { useRecoilState } from 'recoil'
+import { RecoilValueReadOnly, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import { useNavigate } from 'react-router-dom'
 import {
   CareerSelectCode,
   CareerSelectName,
   CareerSubSelectName,
+  CareersItem,
+  CareersItemTypes,
+  CareersState,
   CityName,
+  CareersTypes,
+  JobFamilyCodeCareerInfo,
   JobFamilyName,
+  JobFamilyNameCareerInfo,
   JobName,
+  JobSubCodeCareerInfo,
   JobSubName,
+  JobSubNameCareerInfo,
+  Period,
   RegionName,
 } from '../../atom'
+import JobSelectCarrerInfo from '../../components/Common/JobSelectCareerInfo/JobSelectCareerInfo'
+import JobSubSelectCarrerInfo from '../../components/Common/JobSelectCareerInfo/JobSubSelectCareerInfo'
 
 const CareerInfoContainer = styled.div`
   display: flex;
@@ -47,7 +59,9 @@ const CareerInfoContent = styled.div`
   font-weight: 400;
   color: #666666;
 `
-const CareerInfoLineContainer = styled.div``
+const CareerInfoLineContainer = styled.div`
+  margin-bottom: 20px;
+`
 const JobSearchBtnContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -56,7 +70,7 @@ const JobSearchBtnContainer = styled.div`
 `
 const CareerInfoCategory = styled.div`
   margin-top: 30px;
-  margin-bottom: 5px;
+  margin-bottom: 10px;
   font-size: 18px;
   font-weight: 700;
   color: #666666;
@@ -75,6 +89,16 @@ const SearchButton = styled.button`
     background-color:#c6f0de;
     box-shadow: 0 0 0 1px #c6f0de;
 
+`
+const DeleteButton = styled.button`
+width: 43px;
+height: 43px;
+background-color: white;
+border: 1px solid #76dcb0;
+border-radius: 5px;
+&:hover {
+  background-color:#c6f0de;
+  box-shadow: 0 0 0 1px #c6f0de;
 `
 
 const SlMagnifierContainer = styled.div`
@@ -99,21 +123,52 @@ function CareerInfo() {
   const [inputFavorite, setInputFavorite] = useState<number>() // 선호하는 직종
   const [inputDegrees, setInputDegrees] = useState<number>()
 
-  const [carrerCode, setCarrerCode] = useRecoilState(CareerSelectCode)
-  const [carrerName, setCarrerName] = useRecoilState(CareerSelectName)
-  const [carrerSubName, setCarrerSubName] = useRecoilState(CareerSubSelectName)
+  // * Toggle
+  const [searchJobIsOpen, setSearchJobIsOpen] = useState(false)
+  const [searchRegionIsOpen, setSearchRegionIsOpen] = useState(false)
+  const [searchCareerIsOpen, setSearchCareerIsOpen] = useState(false)
+  // *
 
+  // **** Recoil
+
+  // ** 경력
+
+  const [jobFamilyCodeCareerInfo, setjobFamilyCodeCareerInfo] = useRecoilState(JobFamilyCodeCareerInfo)
+  const [jobFamilyNameCareerInfo, setJobFamilyNameCareerInfo] = useRecoilState(JobFamilyNameCareerInfo)
+  const [jobSubNameCareerInfo, setJobSubNameCareerInfo] = useRecoilState(JobSubNameCareerInfo)
+  const [jobSubCodeCareerInfo, setjobSubCodeCareerInfo] = useRecoilState(JobSubCodeCareerInfo)
+
+  const [carrerSubName, setCarrerSubName] = useRecoilState(CareerSubSelectName)
+  const careers = useRecoilValue<CareersTypes[]>(CareersState)
+  const setCareers = useSetRecoilState<CareersTypes[]>(CareersState)
+  const careersItems = useRecoilValue<CareersItemTypes[]>(CareersItem)
+  const setCareersItems = useSetRecoilState<CareersItemTypes[]>(CareersItem)
+
+  // ** 선호하는 직종
+  const [period, setPeriod] = useRecoilState(Period)
   const [jobFamilyName] = useRecoilState(JobFamilyName)
   const [jobSubName] = useRecoilState(JobSubName)
 
+  // ** 지역
   const [regionName] = useRecoilState(RegionName)
   const [cityName] = useRecoilState(CityName)
 
-  console.log(carrerCode)
+  // ** 학력
+
+  const degreeList = [
+    { degreeCode: 0, name: '학력무관' },
+    { degreeCode: 4, name: '대졸(2~3년)' },
+    { degreeCode: 5, name: '대졸(4년)' },
+    { degreeCode: 6, name: '석사' },
+    { degreeCode: 7, name: '박사' },
+  ]
+
+  // ****
+
+  // ***
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setInputAge(parseInt(e.target.value))
   }
-
   const handlesignalRegist = () => {
     const UserData = {
       age: inputAge,
@@ -136,19 +191,9 @@ function CareerInfo() {
     //   })
     // console.log(FormData)
   }
-  const [searchJobIsOpen, setSearchJobIsOpen] = useState(false)
-  const [searchRegionIsOpen, setSearchRegionIsOpen] = useState(false)
-  const [searchCareerIsOpen, setSearchCareerIsOpen] = useState(false)
+  // ***
 
-  const degreeList = [
-    { degreeCode: 0, name: '학력무관' },
-    { degreeCode: 4, name: '대졸(2~3년)' },
-    { degreeCode: 5, name: '대졸(4년)' },
-    { degreeCode: 6, name: '석사' },
-    { degreeCode: 7, name: '박사' },
-  ]
-
-  // Toggle
+  // * Toggle 함수
   const searchJobToggle = () => {
     setSearchJobIsOpen((searchJobIsOpen) => !searchJobIsOpen)
   }
@@ -158,6 +203,38 @@ function CareerInfo() {
   const searchCareerToggle = () => {
     setSearchCareerIsOpen((searchCareerIsOpen) => !searchCareerIsOpen)
   }
+  // *
+
+  // ** 경력 배열 추가 함수
+  const addCareers = () => {
+    if (jobSubCodeCareerInfo === '' || period === '') {
+      return
+    }
+
+    const career: CareersTypes = {
+      subCode: `${jobSubCodeCareerInfo}`,
+      period: `${period}`,
+    }
+
+    const careerItem: CareersItemTypes = {
+      jobFamily: `${jobFamilyNameCareerInfo}`,
+      job: `${jobSubNameCareerInfo}`,
+      periodName: `${carrerSubName}`,
+    }
+
+    const newTodos = [...careers, career]
+    const newCareersItem = [...careersItems, careerItem]
+
+    setCareers(newTodos)
+    setCareersItems(newCareersItem)
+    setJobFamilyNameCareerInfo('')
+    setJobSubNameCareerInfo('')
+    setCarrerSubName('')
+    setjobFamilyCodeCareerInfo('')
+    setjobSubCodeCareerInfo('')
+    searchCareerToggle()
+  }
+  // **
 
   return (
     <div className="Main-container">
@@ -168,6 +245,7 @@ function CareerInfo() {
           <div style={{ margin: '20px 0 15px 0' }}>
             <CareerInfoLineContainer>
               <CareerInfoCategory>나이</CareerInfoCategory>
+
               <SearchBar
                 width="250px"
                 height="20px"
@@ -229,37 +307,134 @@ function CareerInfo() {
             <CareerInfoLineContainer>
               <CareerInfoCategory>경력</CareerInfoCategory>
             </CareerInfoLineContainer>
-            <SearchCategoryContainer>
-              <div onClick={() => searchCareerToggle()}>
-                <input
-                  style={{
-                    width: '250px',
-                    height: '20px',
-                    fontSize: '15px',
-                    padding: '10px 10px 10px 15px',
-                    margin: '0 15px 0 0',
-                  }}
-                  disabled
-                  value={carrerName}
-                ></input>
-                <input
-                  style={{ width: '250px', height: '20px', fontSize: '15px', padding: '10px 10px 10px 15px' }}
-                  disabled
-                  value={carrerSubName}
-                ></input>
+            {careersItems.map((item, index) => (
+              <div
+                key={index}
+                style={{
+                  display: 'flex',
+
+                  backgroundColor: '',
+                  color: '#666666',
+                  fontSize: '16px',
+                  fontWeight: 500,
+
+                  margin: '10px 0 10px 0',
+                }}
+              >
+                <div>
+                  <input
+                    style={{
+                      width: '200px',
+                      height: '20px',
+                      fontSize: '15px',
+                      padding: '10px 10px 10px 15px',
+                      margin: '0 15px 0 0',
+                    }}
+                    disabled
+                    value={item.jobFamily}
+                  ></input>
+                  <input
+                    style={{
+                      width: '200px',
+                      height: '20px',
+                      fontSize: '15px',
+                      padding: '10px 10px 10px 15px',
+                      margin: '0 15px 0 0',
+                    }}
+                    disabled
+                    value={item.job}
+                  ></input>
+                  <input
+                    style={{
+                      width: '200px',
+                      height: '20px',
+                      fontSize: '15px',
+                      padding: '10px 10px 10px 15px',
+                      margin: '0 15px 0 0',
+                    }}
+                    disabled
+                    value={item.periodName}
+                  ></input>
+                </div>
+                <div>
+                  <DeleteButton
+                    onClick={() => {
+                      const newcareersItems = [...careersItems] // Create a copy of the array
+                      newcareersItems.splice(index, 1) // Remove the clicked item from the copy
+                      setCareersItems(newcareersItems) // Update the state with the new array
+
+                      const newItems = [...careers] // Create a copy of the array
+                      newItems.splice(index, 1) // Remove the clicked item from the copy
+                      setCareers(newItems) // Update the state with the new array
+                    }}
+                  >
+                    <SlMagnifierContainer>
+                      <CgClose size="20px" color="#76dcb0" />
+                    </SlMagnifierContainer>
+                  </DeleteButton>
+                </div>
               </div>
-              <SearchButtonContainer>
-                <SearchButton onClick={() => searchJobToggle()}>
-                  <SlMagnifierContainer>
-                    <SlMagnifier size="20px" color="white" />
-                  </SlMagnifierContainer>
-                </SearchButton>
-              </SearchButtonContainer>
+            ))}
+
+            <SearchCategoryContainer>
+              {careers.length === 3 ? null : (
+                <>
+                  <div onClick={() => searchCareerToggle()}>
+                    <input
+                      style={{
+                        width: '200px',
+                        height: '20px',
+                        fontSize: '15px',
+                        padding: '10px 10px 10px 15px',
+                        margin: '0 15px 0 0',
+                      }}
+                      disabled
+                      value={jobFamilyNameCareerInfo}
+                    ></input>
+                    <input
+                      style={{
+                        width: '200px',
+                        height: '20px',
+                        fontSize: '15px',
+                        padding: '10px 10px 10px 15px',
+                        margin: '0 15px 0 0',
+                      }}
+                      disabled
+                      value={jobSubNameCareerInfo}
+                    ></input>
+
+                    <input
+                      style={{ width: '200px', height: '20px', fontSize: '15px', padding: '10px 10px 10px 15px' }}
+                      disabled
+                      value={carrerSubName}
+                    ></input>
+                  </div>
+                  <SearchButtonContainer>
+                    {searchCareerIsOpen === true ? (
+                      <>
+                        {careers.length === 3 ? null : (
+                          <SearchButton onClick={addCareers}>
+                            <SlMagnifierContainer>
+                              <HiPlus size="20px" color="white" />
+                            </SlMagnifierContainer>
+                          </SearchButton>
+                        )}
+                      </>
+                    ) : (
+                      <SearchButton onClick={() => searchCareerToggle()}>
+                        <SlMagnifierContainer>
+                          <SlMagnifier size="20px" color="white" />
+                        </SlMagnifierContainer>
+                      </SearchButton>
+                    )}
+                  </SearchButtonContainer>
+                </>
+              )}
             </SearchCategoryContainer>
             {searchCareerIsOpen === true ? (
               <div
                 style={{
-                  height: '230px',
+                  // height: '230px',
                   display: 'flex',
                   justifyContent: 'space-between',
                   textAlign: 'center',
@@ -268,7 +443,9 @@ function CareerInfo() {
                   borderRadius: '5px',
                 }}
               >
-                <CareerSelect />
+                <JobSelectCarrerInfo />
+                <JobSubSelectCarrerInfo />
+
                 <CareerSubSelect />
               </div>
             ) : null}
